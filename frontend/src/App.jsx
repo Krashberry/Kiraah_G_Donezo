604ee85285f98732ba235422c1f39430e662bafc
Kiraah <KiraahG@gmail.com>
2025-07-29 02:56:05 -0700

    Have Frontend code in but login is not rendering for some reason.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
-import { QueryClientProvider, QueryClient} from "@tanstack/react-query";
-import MainLayout from "./layouts/main-layout";
-import ProtectedRoute from "./components/protected-route";
-import Login from "./pages/login";
-import Signup from "./pages/signup";
-import Todos from "./pages/todos";
-import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import MainLayout from './layouts/main-layout';
+import ProtectedRoute from './components/protected-route';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import Todos from './pages/todos.jsx';
+import { useEffect } from 'react';
 
 const client = new QueryClient();
 
@@ -13,10 +13,10 @@ function RedirectToLogin() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    navigate("/login")
-  }, [])
+    navigate('/login');
+  }, []);
 
-  return <></>
+  return <></>;
 }
 
 function App() {
@@ -24,12 +24,13 @@ function App() {
     <QueryClientProvider client={client}>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<RedirectToLogin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
